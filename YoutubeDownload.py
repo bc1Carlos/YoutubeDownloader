efe245eac342fbaf797d6902c0e46d5d5cde059f
Carlos Amador <97136831+CharlieChapin@users.noreply.github.com>
2023-03-07 10:26:02 +1000

    Update YoutubeDownload.py

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -1,8 +1,14 @@
 from pytube import YouTube
 
-link = input("Enter a youtube video's URL") # i.e. https://youtu.be/dQw4w9WgXcQ
+link = input("Enter a youtube video's URL:\n>> ") # i.e. https://youtu.be/dQw4w9WgXcQ
 
 yt = YouTube(link)
-yt.streams.filter(file_extension='mp4').first().download()
+title = yt.title
 
-print("downloaded", link)
+video = yt.streams.filter(file_extension='mp4').first()
+audio = yt.streams.filter(only_audio=True).first()
+
+video.download(filename=f'{title}.mp4')
+audio.download(filename=f'{title}.mp3')
+
+print(f'downloaded {title} from {link}')
